0f5aee0d4263dfaae9531711c6258cf65b5e4651
hallgató <hallgat@LABOR115-17>
2017-10-11 09:35:31 +0200

    Add project files.

---
+++ ConsoleApplication11104wovels/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11104wovels
+{
+    class Program
+    {
+        static bool isVowel(char c)
+        {
+            string vowels = "aeiou";
+            for(int i = 0; i < vowels.Length; i++)
+
+            {
+                if (vowels[i] == c) return true;
+            }
+        return false
+                }
+        static void Main(string[] args)
+            string s = " ppl";
+                Console.Writeline(s);
+            int.count = 0;
+            for(int i70; i < s.Length
+        {
+        }
+    }
+}
